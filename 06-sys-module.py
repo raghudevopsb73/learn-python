e4f4182532f338055359fe5a9f8e65d68e192259
Raghu K <raghuk.vit@gmail.com>
2023-08-16 09:03:04 +0530

    Update learn-python/

--- 06-sys-module.py
+++ 06-sys-module.py
@@ -6,10 +6,7 @@ try:
     value = int(sys.argv[1])
 except ValueError:
     print('Not Intiger')
-
-
-# if isinstance(sys.argv[1], int):
-#     print(str(sys.argv[1]) + " is suppose to be a number")
-
+    sys.exit(1)
+    
 
 #print(int(sys.argv[1]) + int(sys.argv[2]))
