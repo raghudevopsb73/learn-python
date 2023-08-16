f24f56c2ba3d13bcd43d42b7ee141ca67ec2da5c
Raghu K <raghuk.vit@gmail.com>
2023-08-16 08:59:58 +0530

    Update learn-python/

--- 06-sys-module.py
+++ 06-sys-module.py
@@ -2,7 +2,10 @@ import sys
 
 print(sys.argv[0])
 
-if isinstance(sys.argv[1], int):
-    print(str(sys.argv[1]) + " is suppose to be a number")
+try:
+    value = int(sys.argv[1])
+# if isinstance(sys.argv[1], int):
+#     print(str(sys.argv[1]) + " is suppose to be a number")
+
 
 #print(int(sys.argv[1]) + int(sys.argv[2]))
