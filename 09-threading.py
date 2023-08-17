6b4b40c81e174adb8ab2f22759b436c86923ab25
Raghu K <raghuk.vit@gmail.com>
2023-08-17 08:03:49 +0530

    Update learn-python/

---
+++ 09-threading.py
@@ -0,0 +1,21 @@
+import threading
+import subprocess
+
+
+def instance_create(node):
+    subprocess.run(["aws", "ec2", "run-instances", "--image-id", "ami-03265a0778a880afb", "--instance-type", "t3.micro",
+                    "--tag-specifications", "ResourceType=instance,Tags=[{Key=Name,Value=" + node + "}]'"],
+                   capture_output=True, text=True)
+
+
+nodes = ["node1", "node2", "node3"]
+threadCount = []
+
+for node in nodes:
+    thread = threading.Thread(target=instance_create(node))
+    threadCount.append(thread)
+    thread.start()
+
+
+for thread in threadCount:
+    thread.join()
