50a3b4c990e639df3f823184471ae1e891bdce08
Raghu K <raghuk.vit@gmail.com>
2023-08-17 08:07:06 +0530

    Update learn-python/

--- 09-threading.py
+++ 09-threading.py
@@ -3,9 +3,10 @@ import subprocess
 
 
 def instance_create(node):
-    out = subprocess.run(["aws", "ec2", "run-instances", "--image-id", "ami-03265a0778a880afb", "--instance-type", "t3.micro",
-                    "--tag-specifications", "ResourceType=instance,Tags=[{Key=Name,Value=" + node + "}]'"],
-                   capture_output=True, text=True)
+    out = subprocess.run(
+        ["aws", "ec2", "run-instances", "--image-id", "ami-03265a0778a880afb", "--instance-type", "t3.micro",
+         "--tag-specifications", "ResourceType=instance,Tags=[{Key=Name,Value=" + node + "}]'"],
+        capture_output=True, text=True)
     print(out.stderr)
 
 
@@ -16,7 +17,6 @@ for node in nodes:
     thread = threading.Thread(target=instance_create(node))
     threadCount.append(thread)
     thread.start()
-
 
 for thread in threadCount:
     thread.join()
