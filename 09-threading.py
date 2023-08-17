8509d0878b5e134eedf94a1340ef5fc0859c8573
Raghu K <raghuk.vit@gmail.com>
2023-08-17 08:07:45 +0530

    Update learn-python/

--- 09-threading.py
+++ 09-threading.py
@@ -5,7 +5,7 @@ import subprocess
 def instance_create(node):
     out = subprocess.run(
         ["aws", "ec2", "run-instances", "--image-id", "ami-03265a0778a880afb", "--instance-type", "t3.micro",
-         "--tag-specifications", "ResourceType=instance,Tags=[{Key=Name,Value=" + node + "}]'"],
+         "--tag-specifications", "'ResourceType=instance,Tags=[{Key=Name,Value=" + node + "}]'"],
         capture_output=True, text=True)
     print(out.stderr)
 
