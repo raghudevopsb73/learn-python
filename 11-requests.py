60daa3ec86d69f03d90e32e16b792bdbf4fd3426
Raghu K <raghuk.vit@gmail.com>
2023-08-17 08:56:14 +0530

    Update learn-python/

--- 11-requests.py
+++ 11-requests.py
@@ -19,22 +19,8 @@ json_data = {
             'name': 'Raghu',
         },
     ],
-    'subject': 'Hello world From Python',
+    'subject': 'Hello world From Python -  1',
     'htmlContent': '<html><head></head><body><p>Hello,</p>This is my first transactional email sent from Brevo.</p></body></html>',
 }
 
-response = requests.post('https://api.brevo.com/v3/smtp/email', headers=headers, json={
-    'sender': {
-        'name': 'Raghu',
-        'email': 'raghuk.vit@gmail.com',
-    },
-    'to': [
-        {
-            'email': 'raghudevopsb72@gmail.com',
-            'name': 'Raghu',
-        },
-    ],
-    'subject': 'Hello world From Python',
-    'htmlContent': '<html><head></head><body><p>Hello,</p>This is my first transactional email sent from Brevo.</p></body></html>',
-})
-print(response.reason)
+response = requests.post('https://api.brevo.com/v3/smtp/email', headers=headers, json=json_data)
