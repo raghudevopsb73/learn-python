dce5572dd5a85a4d9e2f49884bd144f66d5551bb
Raghu K <raghuk.vit@gmail.com>
2023-08-17 08:48:20 +0530

    Update learn-python/

---
+++ 11-requests.py
@@ -0,0 +1,26 @@
+import requests
+import sys
+
+apiKey = sys.argv[1]
+headers = {
+    'accept': 'application/json',
+    'api-key': apiKey,
+    'content-type': 'application/json',
+}
+
+json_data = {
+    'sender': {
+        'name': 'Raghu',
+        'email': 'raghuk.vit@gmail.com',
+    },
+    'to': [
+        {
+            'email': 'raghudevopsb72@gmail.com',
+            'name': 'Raghu',
+        },
+    ],
+    'subject': 'Hello world From Python',
+    'htmlContent': '<html><head></head><body><p>Hello,</p>This is my first transactional email sent from Brevo.</p></body></html>',
+}
+
+response = requests.post('https://api.brevo.com/v3/smtp/email', headers=headers, data=json_data)
