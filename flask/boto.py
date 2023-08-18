1cc96a41373f45870cffb2df339bb254b96495a3
Raghu K <raghuk.vit@gmail.com>
2023-08-18 08:28:13 +0530

    Update learn-python/

--- flask/boto.py
+++ flask/boto.py
@@ -1,6 +1,8 @@
 import boto3
 from botocore.exceptions import ClientError
 import json
+from tabulate import tabulate
+
 
 
 ec2 = boto3.client('ec2', region_name='us-east-1')
@@ -9,4 +11,5 @@ out = ec2.describe_instances()["Reservations"]
 #print(out)
 for instance in out:
     for ami in instance["Instances"]:
-        print(ami["ImageId"])
+        #print(ami["ImageId"])
+        print(tabulate([['InstanceID', ami["InstanceId"]], ['Bob', 19]], tablefmt='orgtbl'))
