dc2d8ab76b4b5b90b8a17b98b7a0482122ada5f3
Raghu K <raghuk.vit@gmail.com>
2023-08-18 08:16:51 +0530

    Update learn-python/

--- flask/boto.py
+++ flask/boto.py
@@ -5,5 +5,5 @@ import json
 
 ec2 = boto3.client('ec2', region_name='us-east-1')
 
-out = ec2.describe_instances()["Reservations"]["Instances"][0]
+out = ec2.describe_instances()["Reservations"]
 print(out)
