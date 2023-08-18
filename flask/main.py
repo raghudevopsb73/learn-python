6a57dbc9bd567f00b6934007b9f193ba58a663b8
Raghu K <raghuk.vit@gmail.com>
2023-08-18 08:04:56 +0530

    Update learn-python/

--- flask/main.py
+++ flask/main.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 app = Flask(__name__,
-            static_url_path='',
+            static_url_path='./',
             static_folder='sample.txt')
 
 app.run(host='0.0.0.0', port=8000)
