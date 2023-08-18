f915ddd2f0ad1e3ad572fcdbb30a5ac7273a880a
Raghu K <raghuk.vit@gmail.com>
2023-08-18 07:55:22 +0530

    Update learn-python/

--- 12-boto.py
+++ 12-boto.py
@@ -1,21 +1,21 @@
 import boto3
 from botocore.exceptions import ClientError
 
-ec2 = boto3.client('ec2',region_name='us-east-1')
+ec2 = boto3.client('ec2', region_name='us-east-1')
 
-ec2.run_instances(
-    ImageId='ami-03265a0778a880afb',
-    InstanceType='t3.small',
-    SecurityGroupIds=["sg-08b043c018c643809"],
-    TagSpecifications=[
-        {
-            'ResourceType': 'instance',
-            'Tags': [
-                {
-                    'Key': 'Name',
-                    'Value': 'test'
-                },
-            ]
-        },
-    ]
-)
+ec2.run_instances(MinCount=1, MaxCount=1,
+                  ImageId='ami-03265a0778a880afb',
+                  InstanceType='t3.small',
+                  SecurityGroupIds=["sg-08b043c018c643809"],
+                  TagSpecifications=[
+                      {
+                          'ResourceType': 'instance',
+                          'Tags': [
+                              {
+                                  'Key': 'Name',
+                                  'Value': 'test'
+                              },
+                          ]
+                      },
+                  ]
+                  )
