401bcd3d56a465a1cd8b15cb16e4a5a6f7f2da39
Raghu K <raghuk.vit@gmail.com>
2023-08-18 08:25:05 +0530

    Update learn-python/

--- flask/boto.py
+++ flask/boto.py
@@ -7,5 +7,5 @@ ec2 = boto3.client('ec2', region_name='us-east-1')
 
 out = ec2.describe_instances()["Reservations"]
 #print(out)
-for instance in out["Instances"]:
-    print(instance["ImageId"])
+for instance in out:
+    print(instance["Instances"])
