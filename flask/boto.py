06a7613c6023621f0bbcff8c38cbd63e662d4ff8
Raghu K <raghuk.vit@gmail.com>
2023-08-18 09:00:41 +0530

    Update learn-python/

--- flask/boto.py
+++ flask/boto.py
@@ -26,7 +26,7 @@ t.align["PrivateIP"] = "l"
 t.align["InstanceType"] = "l"
 for instances in out:
     for instance in instances["Instances"]:
-        t.add_row([instance["InstanceId"], instance["PrivateIpAddress", "-"], instance["InstanceType", "-"] ])
+        t.add_row([instance["InstanceId"], instance.get["PrivateIpAddress", "-"], instance.get["InstanceType", "-"] ])
 
 
 with open('/usr/share/nginx/html/sample.txt', 'w') as w:
