3e4f6b9db89b2e2954cfd182206777111aa2c22f
Raghu K <raghuk.vit@gmail.com>
2023-08-18 08:07:46 +0530

    Update learn-python/

--- flask/main.py
+++ flask/main.py
@@ -2,9 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app = Flask(__name__,
-            static_url_path='',
-            static_folder='./sample.txt')
+
+# app = Flask(__name__,
+#             static_url_path='',
+#             static_folder='./sample.txt')
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
 
 app.run(host='0.0.0.0', port=8000)
-
