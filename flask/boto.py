27c02b9c5957527dc14ea53890ad1f4e5fdd8062
Raghu K <raghuk.vit@gmail.com>
2023-08-18 08:16:19 +0530

    Update learn-python/

--- flask/boto.py
+++ flask/boto.py
@@ -1,7 +1,9 @@
 import boto3
 from botocore.exceptions import ClientError
+import json
+
 
 ec2 = boto3.client('ec2', region_name='us-east-1')
 
-out = ec2.describe_instances()
+out = ec2.describe_instances()["Reservations"]["Instances"]
 print(out)
