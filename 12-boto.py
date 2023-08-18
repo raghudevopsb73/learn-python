ddca9202baf9b28ff786940201024725d62f6fa7
Raghu K <raghuk.vit@gmail.com>
2023-08-18 07:52:17 +0530

    Update learn-python/

--- 12-boto.py
+++ 12-boto.py
@@ -1,7 +1,7 @@
 import boto3
 from botocore.exceptions import ClientError
 
-ec2 = boto3.client('ec2')
+ec2 = boto3.client('ec2',region_name='us-east-1')
 
 ec2.run_instances(
     ImageId='ami-03265a0778a880afb',
