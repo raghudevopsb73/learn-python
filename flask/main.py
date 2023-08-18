996e5a9a0f80f9631a74e82ff94b9b5c85a7c73e
Raghu K <raghuk.vit@gmail.com>
2023-08-18 08:03:14 +0530

    Update learn-python/

--- flask/main.py
+++ flask/main.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Web App with Python Flask!'
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='./')
 
 app.run(host='0.0.0.0', port=8000)
 
