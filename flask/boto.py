31a5577d4a565c918f15e8fe2e08c1271ec02fd3
Raghu K <raghuk.vit@gmail.com>
2023-08-18 08:41:58 +0530

    Update learn-python/

--- flask/boto.py
+++ flask/boto.py
@@ -2,18 +2,27 @@ import boto3
 from botocore.exceptions import ClientError
 import json
 from tabulate import tabulate
+from prettytable import PrettyTable
 
+ec2 = boto3.client('ec2', region_name='us-east-1')\\
 
+# instance_list = []
+# out = ec2.describe_instances()["Reservations"]
+# #print(out)
+#
+# for instances in out:
+#     for instance in instances["Instances"]:
+#         instance_list.append([instance["InstanceId"], instance["PrivateIpAddress"], instance["InstanceType"] ])
+#
+#         # instance_list.append(tabulate([
+#         #     [instance["InstanceId"], instance["PrivateIpAddress"], instance["InstanceType"] ],
+#         # ], tablefmt='orgtbl'))
+# print(tabulate(instance_list, tablefmt='orgtbl'))
 
-ec2 = boto3.client('ec2', region_name='us-east-1')
-instance_list = []
+t = PrettyTable(['InstanceID', 'PrivateIP', 'InstanceType'])
 out = ec2.describe_instances()["Reservations"]
-#print(out)
 for instances in out:
     for instance in instances["Instances"]:
-        instance_list.append([instance["InstanceId"], instance["PrivateIpAddress"], instance["InstanceType"] ])
+        t.add_row([instance["InstanceId"], instance["PrivateIpAddress"], instance["InstanceType"] ])
 
-        # instance_list.append(tabulate([
-        #     [instance["InstanceId"], instance["PrivateIpAddress"], instance["InstanceType"] ],
-        # ], tablefmt='orgtbl'))
-print(tabulate(instance_list, tablefmt='orgtbl'))
+print(t)
