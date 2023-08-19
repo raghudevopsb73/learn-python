86b39bb2ecab635bb7d5ff350d6ae1fcdcf71635
Raghu K <raghuk.vit@gmail.com>
2023-08-19 07:56:32 +0530

    Update learn-python/

---
+++ lambda-email/lambda.py
@@ -0,0 +1,29 @@
+import json
+import requests
+import sys
+
+def lambda_handler(event, context):
+    apiKey = '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'
+    headers = {
+        'accept': 'application/json',
+        'api-key': apiKey,
+        'content-type': 'application/json',
+    }
+
+    json_data = {
+        'sender': {
+            'name': 'Raghu',
+            'email': 'raghuk.vit@gmail.com',
+        },
+        'to': [
+            {
+                'email': 'raghudevopsb72@gmail.com',
+                'name': 'Raghu',
+            },
+        ],
+        'subject': 'Hello world From Python -  1',
+        'htmlContent': '<html><head></head><body><p>Hello,</p>This is my first transactional email sent from Brevo.</p></body></html>',
+    }
+
+    response = requests.post('https://api.brevo.com/v3/smtp/email', headers=headers, json=json_data)
+    return response
